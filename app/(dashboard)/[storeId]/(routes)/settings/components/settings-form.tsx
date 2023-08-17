b4be5bbc7db4838b1f0ea312d551e349e5db4d7d
Shaan <shaanduss@gmail.com>
2023-08-17 10:38:46 +0400

    billboard start setup

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -17,6 +17,7 @@ import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -34,6 +35,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 
   const params = useParams()
   const router = useRouter()
+  const origin = useOrigin()
 
   const [open, setOpen] = useState(false)
   const [loading, setLoading] = useState(false)
@@ -128,7 +130,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
       <Separator />
       <ApiAlert
       title = "NEXT_PUBLIC_API_URL"
-      description = "test-dec"
+      description = {`${origin}/api/${params.storeId}}`}
       variant="public"
       />
     </>
